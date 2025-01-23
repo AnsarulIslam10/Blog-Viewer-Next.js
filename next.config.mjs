2a7b95d07fa58a2fc5d6274730aec8a5564f49c9
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-23 23:43:40 +0600

    issue solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com"], // Add the allowed image host here
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,4 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Profile = async () => {
@@ -15,14 +14,9 @@ const Profile = async () => {
       <div>
         <h3 className="text-3xl text-center my-8">Welcome to your profile</h3>
         <div className="flex flex-col justify-center items-center">
-          {user?.picture ? (
-            <Image src={user.picture} alt="pfp" height={80} width={80} className="rounded-full border-4 border-cyan-500 " />
-          ) : (
-            <p className="inline-block px-4 py-2 rounded-full border-4 text-4xl border-cyan-500">
-              {user.given_name.slice(0, 1)}
-            </p>
-          )}
-          <h1 className="text-xl">{user?.given_name} {user?.family_name}</h1>
+          <h1 className="text-xl">
+            {user?.given_name} {user?.family_name}
+          </h1>
           <p>{user?.email}</p>
         </div>
       </div>
