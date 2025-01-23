cfc29d382b9421065b5b4312aecb645b9dfbdead
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-23 23:01:07 +0600

    all post card on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const Home = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
   return (
-    <div>
-      
+    <div className="mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {data.map((item) => (
+          <Link
+            key={item.id}
+            href={`blog/${item?.id}`}
+            className="border shadow-sm border-t-4 border-t-cyan-500 hover:scale-105 transition-all duration-300 p-2 text-center flex justify-center items-center "
+          >
+            <p>{item.title}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
